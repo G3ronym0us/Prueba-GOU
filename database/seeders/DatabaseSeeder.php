9553b48500a0df6df24a162dbd8a95ee85e43cc2
Diohandres <diohandres1703@hotmail.com>
2021-01-07 18:25:26 -0400

    Seeders Up

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,10 +14,10 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
-        //$this->call(BrandSeeder::class);
-        //$this->call(UserSeeder::class);
-        //$this->call(VirtualWalletDriverSeeder::class);
-        //$this->call(NotificationSeeder::class);
+        $this->call(BrandSeeder::class);
+        $this->call(UserSeeder::class);
+        $this->call(VirtualWalletDriverSeeder::class);
+        $this->call(NotificationSeeder::class);
         $this->call(PushNotificationSeeder::class);
     }
 }
